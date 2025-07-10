e456f6f8411b97c7e9f72972e9c7298db1d5de59
unknown <parfaittelie@outlook.fr>
2025-07-11 00:28:39 +0200

    création des tables Marque, voiture et avis

---
+++ src/Entity/Voiture.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VoitureRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VoitureRepository::class)]
+class Voiture
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 55)]
+    private ?string $modele = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $immatriculation = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $energie = null;
+
+    #[ORM\Column(length: 55)]
+    private ?string $couleur = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTime $date_premiere_immatriculation = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getModele(): ?string
+    {
+        return $this->modele;
+    }
+
+    public function setModele(string $modele): static
+    {
+        $this->modele = $modele;
+
+        return $this;
+    }
+
+    public function getImmatriculation(): ?string
+    {
+        return $this->immatriculation;
+    }
+
+    public function setImmatriculation(string $immatriculation): static
+    {
+        $this->immatriculation = $immatriculation;
+
+        return $this;
+    }
+
+    public function getEnergie(): ?string
+    {
+        return $this->energie;
+    }
+
+    public function setEnergie(string $energie): static
+    {
+        $this->energie = $energie;
+
+        return $this;
+    }
+
+    public function getCouleur(): ?string
+    {
+        return $this->couleur;
+    }
+
+    public function setCouleur(string $couleur): static
+    {
+        $this->couleur = $couleur;
+
+        return $this;
+    }
+
+    public function getDatePremiereImmatriculation(): ?\DateTime
+    {
+        return $this->date_premiere_immatriculation;
+    }
+
+    public function setDatePremiereImmatriculation(\DateTime $date_premiere_immatriculation): static
+    {
+        $this->date_premiere_immatriculation = $date_premiere_immatriculation;
+
+        return $this;
+    }
+}
